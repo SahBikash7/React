10c2f062b5817fa3c31422148a440eb7edfcaf71
Bikash <sahb2676@gmail.com>
2024-01-26 09:06:55 +0545

    Made a Smaller than Smallest Custom-React

--- 01-Basics-Vite/src/App.jsx
+++ 01-Basics-Vite/src/App.jsx
@@ -1,14 +1,21 @@
 import Intro from "./Intro";
 function App() {
+  const username = "Bikash";
   return (
     // We cannot return multiple elements :
     <>
       <Intro />
-      <h1>Hello World | Bikash</h1>
+      <h1>Hello World | {username}</h1>
     </>
 
     // We had to wrap the elements into a single <div></div> every time we return since we cannot return more than 1 element.As a result the fragment was made :
     // <> ... </> is known as fragments.
+
+    // {...Evaluated-Expression only here...}
+    // So inside the curly braces we don't write JavaScript we only write the final output of the JavaScript...
+
+    // Why we only write evaluated expression inside the curly braces??
+    // => The things we return are at the end of the day converted into the object ( see main.jsx line 20-29 ) now we don't write the if-else or any JavaScript directly inside the object, yah u can write javaScript inside a function declared into the object but React here directly inserts the variable as it is inside the object so no JS is written inside the curly braces and hence it is called Evaluated-Expression.
   );
 }
 export default App;
--- 01-Basics-Vite/src/main.jsx
+++ 01-Basics-Vite/src/main.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// function MyApp() {
+//   return <h3>Hello,This is Custom App!</h3>;
+// }
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click To Open Google",
+// };
+
+// The reactElement doesn't work because tyo element lai render garna hamileh aafnai customRender banako them 02-Custom-React ma but here we are using the render of the react aaba yo render leh aafnai syntax haru expect garxa ,simply speaking hmro reactElement ko syntax react ko render method leh bujhena..
+
+// const username = "Manish";
+// const reactElement = React.createElement(
+//   "a",
+//   {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   "Click To Visit Google!!!",
+//   username
+// );
+
+//The reactElement works now because to render the reactElement  we are using the render of the react and now reactElement's syntax is as expected by the render of react.
+
+// const anotherElement = (
+//   <a href="https://facebook.com" target="_nothing">
+//     Click To Visit Facebook
+//   </a>
+// );
+// Also This Work
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
