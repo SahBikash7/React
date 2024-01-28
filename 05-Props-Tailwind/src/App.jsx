e63cbcdeccd5046f05ea31694c5d78c3347e02b3
Bikash <sahb2676@gmail.com>
2024-01-28 23:24:23 +0545

    Props and Tailwind

---
+++ 05-Props-Tailwind/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Card from "./components/Card";
+function App() {
+  // const myObj = {
+  //   name: "Hritik",
+  //   age: 34,
+  // };
+  // const myArr = [1, 2, 3, 4, 5];
+
+  return (
+    <>
+      {/* class is a reserved keyword in .jsx so to use our CSS Wala class we use className and also remember that in .jsx file every html tag should be closed*/}
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+      {/* <Card username="Bikash" someObject={myObj} someArr={myArr} /> */}
+      <Card username="Bikash" />
+      <Card username="Aayush" />
+      <Card />
+    </>
+  );
+}
+
+export default App;
